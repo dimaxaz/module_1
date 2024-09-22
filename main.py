3f0f4d9dbc8b7a5f8a6b84f1d8ad199aacc6ec23
dimaxaz <145430196+dimaxaz@users.noreply.github.com>
2024-09-22 17:11:40 +0300

    Переменные

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-example = 'Russia'
-
-print(example[0])
-print(example[3::1])
-print(example[::-1])
-print(example[::2])
+task_complit = 12
+task_time = 1.5
+course_name = 'Python'
+time_for_one_task =  task_time/task_complit
+print("Курс: ", course_name,
+      "\nвсего задач: ", task_complit,
+      "\nзатрачено часов: ", task_time,
+      "\nсреднее время выполнения: ",time_for_one_task)
