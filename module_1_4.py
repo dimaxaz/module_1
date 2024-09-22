5d66c7fa252c52f342da926614600717bf4e8dfd
dimaxaz <145430196+dimaxaz@users.noreply.github.com>
2024-09-22 18:44:56 +0300

    task_4_re Организация программ и методы строк

--- module_1_4.py
+++ module_1_4.py
@@ -1,7 +1,8 @@
 my_string = input("Введите что-нибудь: ")
 
-print(my_string .upper(),
-      my_string.lower(),
-      my_string.replace(" ", ""),
-      my_string[0],
-      my_string[-1])
+print(len(my_string))
+print(my_string .upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
