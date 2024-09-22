910fecdefd76f54cf735428ea04180340e2ea5aa
dimaxaz <145430196+dimaxaz@users.noreply.github.com>
2024-09-22 17:28:56 +0300

    task_2 Переменные

---
+++ module_1_2.py
@@ -0,0 +1,8 @@
+task_complit = 12
+task_time = 1.5
+course_name = 'Python'
+time_for_one_task =  task_time/task_complit
+print("Курс: ", course_name,
+      "\nвсего задач: ", task_complit,
+      "\nзатрачено часов: ", task_time,
+      "\nсреднее время выполнения: ",time_for_one_task)
